e05aa20c75fa2af85eb3cf78b7cbb9ad3e3a793c
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-19 20:02:33 +0300

    complete labs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
   const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+  const [size, setSize] = useState('small');
+  const [contactInfo, setContactInfo] = useState('');
+  const [orderIsSubmitted, setOrderIsSubmitted] = useState(false);
+  function togglePepperoni(e) {
+    setPepperoniIsChecked(e.target.checked);
+  }
 
-  const togglePepperoni = (e) => setPepperoniIsChecked(e.target.checked);
-
+  function selectSize(e) {
+    setSize(e.target.value);
+  }
+  function updateContactField(e) {
+    setContactInfo(e.target.value);
+  }
+  function submitOrder(e) {
+    e.preventDefault();
+    setOrderIsSubmitted(true);
+  }
   return (
     <div>
       <h1>Place an Order</h1>
-      <form>
+      <p>
+        Your selection: {size} {pepperoniIsChecked ? 'pepperoni' : 'cheese'}
+      </p>
+      <form onSubmit={submitOrder}>
+        <h3>Contact Info</h3>
+        <label htmlFor="email">Enter your email address: </label>
+        <input
+          type="text"
+          value={contactInfo}
+          id="email"
+          placeholder="email address"
+          onChange={updateContactField}
+        />
         <div>
           <h3>Toppings</h3>
           <input
@@ -19,8 +45,17 @@ function App() {
             onChange={togglePepperoni}
           />
           <label htmlFor="pepperoni">Add pepperoni</label>
+          <h3>Size</h3>
+          <label htmlFor="select-size">Select size: </label>
+          <select id="select-size" value={size} onChange={selectSize}>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
         </div>
+        <button type="submit">Submit Order</button>
       </form>
+      {orderIsSubmitted ? <h2>Thanks for your order!</h2> : null}
     </div>
   );
 }
